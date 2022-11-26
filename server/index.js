b1a30124ec255d1e98bffc37d1012be7eb108a68
kontobaf <kontobaf@mail.ru>
2022-11-26 12:42:27 +0500

    controllers + routers + ApiError

--- server/index.js
+++ server/index.js
@@ -4,12 +4,17 @@ const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
 const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
+
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use('/api', router)
 
 
 
