13be990e7096b81af1a804759fd4ed3f8aec624f
kontobaf <kontobaf@mail.ru>
2022-11-27 14:09:31 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,9 +15,9 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use('/api', router)
+
+
 app.use(errorHandler)
-
-
 
 
 const start = async () => {
