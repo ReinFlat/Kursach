5a915a0e5d745e31d21489bbefc10efc6e8c812d
kontobaf <kontobaf@mail.ru>
2022-12-01 23:29:00 +0500

    Update UserRouter.js

--- server/routes/UserRouter.js
+++ server/routes/UserRouter.js
@@ -1,11 +1,12 @@
 const Router = require('express')
 const router = new Router()
 const UserController = require('../controllers/UserController')
+const authMiddleware = require('../middleware/authMiddleware')
 
 
 router.post('/registration', UserController.registration)
 router.post('/login', UserController.login)
-router.get('/auth', UserController.check)
+router.get('/auth', authMiddleware,UserController.check)
 router.get('/test', (req, res) => {
     res.json({message: 'ALL WORKING!!'})
 })
