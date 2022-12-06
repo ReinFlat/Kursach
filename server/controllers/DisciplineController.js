513b208c33741cb6611081b4668b12577248531c
kontobaf <kontobaf@mail.ru>
2022-12-06 13:26:04 +0500

    Update DisciplineController.js

--- server/controllers/DisciplineController.js
+++ server/controllers/DisciplineController.js
@@ -4,12 +4,12 @@ const ApiError = require('../error/ApiError');
 class DisciplineController {
     async create(req, res) {
         const {Disc_name} = req.body
-        const discipline = await discipline.create({Disc_name})
-        return res.json(discipline)
+        const discipline = await Discipline.create({Disc_name})
+        return res.json(Discipline)
     }
 
     async getAll(req, res) {
-        const disciplines = await discipline.findAll()
+        const disciplines = await Discipline.findAll()
         return res.json(disciplines)
     }
 
