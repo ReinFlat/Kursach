996f91b99ca2898ff16a48a02e53963059e00cd3
kontobaf <kontobaf@mail.ru>
2022-12-06 13:00:36 +0500

    Update UserController.js

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -23,7 +23,7 @@ class UserController {
         }
         const hashPassword = await bcrypt.hash(password, 5)
         const user = await User.create({email, role, password: hashPassword})
-        const Basket = await Basket.create({userId: user.id})
+        const basket = await Basket.create({userId: user.id})
         const token = generateJwt(user.id, user.email, user.role)
         return res.json({token})
     }
