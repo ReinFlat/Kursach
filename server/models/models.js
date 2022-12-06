5555a824916abcff54a117b3347c4121e36dcd50
kontobaf <kontobaf@mail.ru>
2022-12-06 12:54:24 +0500

    Update models.js

--- server/models/models.js
+++ server/models/models.js
@@ -5,7 +5,7 @@ const User = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     email: {type: DataTypes.STRING, unique: true},
     password: {type: DataTypes.STRING},
-    role: {type: DataTypes.STRING, defaultValue: 'Сотрудник'}
+    role: {type: DataTypes.STRING, defaultValue: 'STUDENT'}
 })
 
 
