ec575a2cd8444947cb1cd0ad63396babd24f21a9
ReinFlat <tomato535@yandex.ru>
2022-12-15 20:11:48 +0500

    api

--- client/src/http/lessonAPI.js
+++ client/src/http/lessonAPI.js
@@ -1,12 +1,12 @@
 import {$authHost, $host} from "./index";
 
 export const createLesson = async (lesson) => {
-    const {data} = await $authHost.post('/lesson/LessonCreate', lesson);
+    const {data} = await $authHost.post('api/lesson/LessonCreate', lesson);
     return data;
 }
 
 export const getAll = async () => {
-    const {data} = await $host.get('/lesson/LessonGet');
+    const {data} = await $host.get('api/lesson/LessonGet');
     return data;
 }
 
