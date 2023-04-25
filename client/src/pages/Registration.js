61546c4ff7570ff15209895e32e1ecb7ead98ce5
ReinFlat <tomato535@yandex.ru>
2023-04-25 15:42:12 +0500

    Добавил круг
    добавил круг поиска нового места для офиса, оформил шоб красиво было

--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -1,11 +1,11 @@
 import { useEffect, useState} from "react";
-import { Button, ButtonGroup, Card, Col, Container, Row} from "react-bootstrap";
+import { ButtonGroup, Card, Col, Container, Row} from "react-bootstrap";
 import CreateCompany from "../components/modals/CreateCompany";
 import CreateStudent from "../components/modals/CreateStudent";
 import AdminTable from "../components/Profile/Admin/AdminTable";
 import { getCompany } from "../http/adminAPI";
 import { getAddresses } from "../http/adminAPI";
-import { YMaps, Map, Circle, Placemark } from '@pbe/react-yandex-maps';
+import { YMaps, Map, Circle, Placemark, Button} from '@pbe/react-yandex-maps';
 import MyPlacemark from "../components/MyPlacemark";
 import AddressCard from "../components/AddressCard";
 
@@ -15,6 +15,12 @@ const Registration = () => {
     const [companys, setCompanys] = useState([]);
     const [addresses, setAddresses] = useState([]);
     const [placeId, setPlaceId] = useState();
+    const [center, setCenter] = useState([55.76, 37.64]); // начальные координаты центра круга
+    const [addCircle, setAddCircle] = useState(false);
+
+    const handleCircleDrag = (event) => {
+        setCenter(event.originalEvent.target.geometry.getCoordinates()); // обновляем координаты центра круга при его перетаскивании
+    };
 
     useEffect(()=> {
         getCompany().then((data) => {
@@ -57,7 +63,42 @@ const Registration = () => {
 
                         </Col>
                         <Col>
-                            <Map width={800} height={650} defaultState={{ center: [57.150417, 65.548863], zoom: 14 }}>
+                            <Container style={{ width:800, height:680}}>
+                            <Map width={800} height={650} 
+                            defaultState={{ center: [57.150417, 65.548863], zoom: 14 }}
+                            onActionEnd={(event) => {
+                                setCenter(event.get('target').getCenter()); // обновляем координаты центра карты при ее перемещении
+                              }}>
+                                {
+                                    addCircle 
+                                    ?
+                                    <>
+                                        <Button
+                                        options={{ selectOnClick: false, maxWidth: 800 }}
+                                        onClick={() => setAddCircle(false)}
+                                        data={{ content: `Центр круга: ${center.join(', ')}` }}/>
+
+                                        <Circle
+                                            options={{
+                                                draggable: true,
+                                                fillColor: '#cbcbcb77',
+                                                strokeColor: '#8b8b8b',
+                                                strokeOpacity: 0.8,
+                                                strokeWidth: 5,
+                                                draggable: true
+                                            }}
+                                            geometry={[center, 1000]}
+                                            onDragEnd={handleCircleDrag}
+                                        />
+                                    </>
+                                    :
+                                        <Button
+                                        options={{ selectOnClick: false, maxWidth: 800 }}
+                                        onClick={() => setAddCircle(true)}
+                                        data={{ content:"✚" }}/>
+
+                                }
+                            
                             <Circle geometry={[[57.150417, 65.548863], 1000]} />
                             <Placemark geometry={[57.150417, 65.548863]} 
                             options={{
@@ -73,6 +114,7 @@ const Registration = () => {
                                 <MyPlacemark placeId={placeId} key={address.user_id} address={address} />)
                             }
                             </Map>
+                            </Container>
                         </Col>
                     </Row>
                 </Card>
